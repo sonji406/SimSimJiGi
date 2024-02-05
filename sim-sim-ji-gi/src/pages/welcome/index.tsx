dc64bfb0a3aa2f3a3b2270018ff33f2bbdf04b8c
sonji406 <wkaqh24@gmail.com>
2024-02-05 22:24:32 +0900

    Feat: 환영 화면 구성 약간 수정

--- sim-sim-ji-gi/src/pages/welcome/index.tsx
+++ sim-sim-ji-gi/src/pages/welcome/index.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function Welcome() {
   return (
-    <div className='text-center'>
+    <div>
       <Link href={'welcome/intro'}>
-        <div className='w-screen h-screen'>심심지기</div>
+        <div className='w-screen h-screen text-center'>심심지기</div>
       </Link>
     </div>
   );
